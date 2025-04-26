8eadabbfbea32e3afb53d2fdbd873f2dbdb7a251
SachinPatil09 <sachinspatil0999@gmail.com>
2025-04-26 12:25:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  let width = window.screen.width;
+  let height = window.screen.height;
+  document.getElementById("screenInfo").innerText = width + " x " + height;
+};
